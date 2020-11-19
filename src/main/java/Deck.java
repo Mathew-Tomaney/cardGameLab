3ce42452dd1535bbb11464aea5b46fbeb56c5ba6
Mathew-Tomaney <mat.tomaney@gmail.com>
2020-11-19 15:33:45 +0000

    add test to check filled deck is in expected order. pass.

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -20,4 +20,8 @@ public class Deck {
         }
     }
 
+    public Card getFirstCard(){
+        return this.cards.get(0);
+    }
+
 }
--- src/test/java/DeckTest.java
+++ src/test/java/DeckTest.java
@@ -5,10 +5,12 @@ import static junit.framework.TestCase.assertEquals;
 
 public class DeckTest {
     Deck deck;
+    Card card;
 
     @Before
     public void before() {
         deck = new Deck();
+        card = new Card(SuitType.SPADES,RankType.ACE);
     }
 
     @Test
@@ -20,5 +22,12 @@ public class DeckTest {
     public void deckFull(){
         deck.fillDeck();
         assertEquals(52, deck.cardsInDeck());
+    }
+
+    @Test
+    public void deckIsOrdered(){
+        deck.fillDeck();
+        assertEquals(RankType.ACE, deck.getFirstCard().getRank());
+        assertEquals(SuitType.SPADES, deck.getFirstCard().getSuit());
     }
 }
