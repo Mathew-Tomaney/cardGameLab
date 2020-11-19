23221ef9fe3a5a263bf5c8f353ee88e9a24d77cf
Mathew-Tomaney <mat.tomaney@gmail.com>
2020-11-19 14:22:34 +0000

    add tests for card properties

--- src/main/java/SuitType.java
+++ src/main/java/SuitType.java
@@ -1,2 +1,6 @@
 public enum SuitType {
+    SPADES,
+    CLUBS,
+    DIAMONDS,
+    HEARTS
 }
--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -1,2 +1,26 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CardTest {
+    Card card;
+
+    @Before
+    public void before() {
+        card = new Card(SuitType.SPADES, RankType.FIVE);
+    }
+
+    @Test
+    public void canGetSuit(){
+        assertEquals(SuitType.SPADES, card.getSuit());
+    }
+    @Test
+    public void canGetRank(){
+        assertEquals(RankType.FIVE, card.getRank());
+    }
+    @Test
+    public void canGetValue(){
+        assertEquals(5, card.getRankValue());
+    }
 }
