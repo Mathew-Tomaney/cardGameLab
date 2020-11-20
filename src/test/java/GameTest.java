2ee5b0c5472efab7e0d04454c512e81e03e92475
Mathew-Tomaney <mat.tomaney@gmail.com>
2020-11-20 16:27:36 +0000

    add test to retrieve player list from Game class

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -4,4 +4,24 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class GameTest {
+    Dealer dealer;
+    Player player;
+    Card card;
+    Deck deck;
+    Game game;
+
+    @Before
+    public void before(){
+        game = new Game();
+        dealer = new Dealer();
+        player = new Player("Mat");
+        card = new Card(SuitType.SPADES, RankType.ACE);
+        deck = new Deck();
+    }
+
+    @Test
+    public void canAddPlayer(){
+        game.addPlayer();
+        assertEquals(1, game.getPlayers().size());
+    }
 }
